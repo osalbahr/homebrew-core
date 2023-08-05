a28cf35d07414f685832e0973a3894ab63254eaf
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2023-08-04 21:29:54 -0400

    openmodelica 1.21.0

--- Formula/openmodelica.rb
+++ Formula/openmodelica.rb
@@ -3,10 +3,9 @@ class Openmodelica < Formula
   homepage "https://openmodelica.org/"
   # GitHub's archives lack submodules, must pull:
   url "https://github.com/OpenModelica/OpenModelica.git",
-      tag:      "v1.18.0",
-      revision: "49be4faa5a625a18efbbd74cc2f5be86aeea37bb"
+      tag:      "v1.21.0",
+      revision: "ba0088488ec73bbadcc6d79a25da7aaa3e980585"
   license "GPL-3.0-only"
-  revision 6
   head "https://github.com/OpenModelica/OpenModelica.git", branch: "master"
 
   bottle do
