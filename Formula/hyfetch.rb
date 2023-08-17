c0f5a060a3ac668caf37dd89cddfb452808c5f86
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2023-08-17 16:31:19 -0400

    hyfetch 1.4.10 (new formula)

---
+++ Formula/hyfetch.rb
@@ -0,0 +1,24 @@
+class Hyfetch < Formula
+  desc "Maintained fork of Neofetch"
+  homepage "https://github.com/dylanaraps/neofetch"
+  url "https://github.com/hykilpikonna/hyfetch/archive/refs/tags/1.4.10.tar.gz"
+  sha256 "a8988b081c79a4a80d92262e3588fab1ad7f890ff6dbfb4ed76f697be1f8d903"
+  license "MIT"
+  head "https://github.com/dylanaraps/neofetch.git", branch: "master"
+
+  on_macos do
+    depends_on "screenresolution"
+  end
+
+  conflicts_with "neofetch", because: "conflicting name"
+
+  def install
+    inreplace "neofetch", "/usr/local", HOMEBREW_PREFIX
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/neofetch", "--config", "none", "--color_blocks", "off",
+                              "--disable", "wm", "de", "term", "gpu"
+  end
+end
