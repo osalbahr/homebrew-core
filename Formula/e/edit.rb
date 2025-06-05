9e6f51dfdd673b381afa10dae9790f108948762a
Luke Thomas <96008479+Zoobdude@users.noreply.github.com>
2025-06-05 13:20:26 +0100

    edit 1.1.0 (new formula)

---
+++ Formula/e/edit.rb
@@ -0,0 +1,18 @@
+class Edit < Formula
+  desc "A simple text editor with clickable interface"
+  homepage "https://github.com/microsoft/edit"
+  url "https://github.com/microsoft/edit/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "d91205513b245bf4ed1127c35d148cac4f7dafd22b071fe3443d080bbda4b9ef"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    ENV["RUSTC_BOOTSTRAP"] = "1"
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system bin/"edit", "--version"
+  end
+end
